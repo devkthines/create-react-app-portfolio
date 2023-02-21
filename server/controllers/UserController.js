34b2aef83eaa46755f886245eb1a192b9d12b953
dev.kthines <dev.kthines@gmail.com>
2023-02-21 13:17:02 -0800

    first commit

---
+++ server/controllers/UserController.js
@@ -0,0 +1,20 @@
+﻿import Project from "../models/UserModel.js";
+import Contact from "../models/ContactModel.js";
+export const getProjects = async (req, res) => {
+    try {
+        const users = await Project.find();
+        res.json(users);
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+export const saveMessage = async (req, res) => {
+    const user = new Contact(req.body);
+    try {
+        const inserteduser = await user.save();
+        res.status(201).json(inserteduser);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
---
+++ server/routes/UserRoute.js
@@ -0,0 +1,12 @@
+﻿import express from "express";
+import {
+    getProjects, saveMessage,
+
+} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get('/projects', getProjects);
+router.post('/contact', saveMessage);
+
+export default router;
